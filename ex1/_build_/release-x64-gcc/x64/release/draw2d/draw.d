bb9ce6ca336f7b86f9b1846c798ca501713e2fdd
Brooklyn Mcswiney <brooklynmcswiney@gmail.com>
2023-10-10 15:46:17 +0100

    initial set up with ex1

---
+++ ex1/_build_/release-x64-gcc/x64/release/draw2d/draw.d
@@ -0,0 +1,10 @@
+../_build_/release-x64-gcc/x64/release/draw2d/draw.o: draw.cpp draw.hpp \
+ forward.hpp ../vmlib/vec2.hpp color.hpp color.inl surface.hpp \
+ surface.inl
+draw.hpp:
+forward.hpp:
+../vmlib/vec2.hpp:
+color.hpp:
+color.inl:
+surface.hpp:
+surface.inl:
---
+++ ex1/draw2d/draw.cpp
@@ -0,0 +1,43 @@
+#include "draw.hpp"
+
+#include <algorithm>
+
+#include <cmath>
+
+#include "color.hpp"
+#include "surface.hpp"
+
+void draw_rectangle_solid( Surface& aSurface, Vec2f aMinCorner, Vec2f aMaxCorner, ColorU8_sRGB aColor )
+{	
+	for (int y = aMinCorner.y; y < aMaxCorner.y; y++)
+	{
+		for (int x = aMinCorner.x; x < aMaxCorner.x; x++)
+		{
+			aSurface.set_pixel_srgb(x, y, aColor);
+		}
+	}
+}
+
+void draw_rectangle_outline( Surface& aSurface, Vec2f aMinCorner, Vec2f aMaxCorner, ColorU8_sRGB aColor )
+{
+	//TODO: your implementation goes here
+	//TODO: your implementation goes here
+	//TODO: your implementation goes here
+
+	for (int y = aMinCorner.y; y <= aMaxCorner.y; y++)
+	{
+		for (int x = aMinCorner.x; x <= aMaxCorner.x; x++)
+		{
+			if(x == aMinCorner.x || x == aMaxCorner.x || y == aMinCorner.y || y == aMaxCorner.y)
+			{
+				aSurface.set_pixel_srgb(x, y, aColor);
+			}
+		}
+	}
+
+	//TODO: remove the following when you start your implementation
+	(void)aSurface; // Avoid warnings about unused arguments
+	(void)aMinCorner;
+	(void)aMaxCorner;
+	(void)aColor;
+}

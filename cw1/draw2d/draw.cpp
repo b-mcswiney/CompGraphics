56d0f27754030b1e975e2b2c6dfe28f4b17e7b1c
Brooklyn Mcswiney <brooklynmcswiney@gmail.com>
2023-10-23 16:57:06 +0100

    fixed line drawing. Need to implement clipping

--- cw1/draw2d/draw.cpp
+++ cw1/draw2d/draw.cpp
@@ -10,38 +10,36 @@
 
 void draw_line_solid( Surface& aSurface, Vec2f aBegin, Vec2f aEnd, ColorU8_sRGB aColor )
 {
-	// If end is behind beginning swap them
-	if (aEnd.x < aBegin.x) {
-		std::swap(aEnd, aBegin);
-	}
-
-	// Find vector d
+	// Calculate D vector
 	float dX = aEnd.x - aBegin.x;
 	float dY = aEnd.y - aBegin.y;
 
-	float offset = 0;
-	float t = 0;
+	// initialize step
+	int step;
 
-	// X major
-	if(dX > dY) {
-		t = dY / dX;
-
-		for (int i = aBegin.x; i <= aEnd.x; i++) { 
-			aSurface.set_pixel_srgb(i, aBegin.y + offset, aColor);
-			offset += t;
-		}
+	// Finding X major or Y Major
+	if(abs(dX) > abs(dY)) {
+		step = abs(dX);
 	}
-	// Y major
 	else {
-		t = dX / dY;
-
-		for (int i = aBegin.y; i <= aEnd.y; i++) { 
-			aSurface.set_pixel_srgb(aBegin.x, i + offset, aColor);
-			offset += t;
-		}
+		step = abs(dY);
 	}
 
+	// Find step in each iteration
+	float yInc = dY/step;
+	float xInc = dX/step;
 
+	//initialize current x and y values
+	float currentY = aBegin.y;
+	float currentX = aBegin.x;
+
+	// Set pixels in the line
+	for(int i = 0; i < step; i++)
+	{
+		aSurface.set_pixel_srgb(currentX, currentY, aColor);
+		currentY += yInc;
+		currentX += xInc;
+	}
 }
 
 void draw_triangle_wireframe( Surface& aSurface, Vec2f aP0, Vec2f aP1, Vec2f aP2, ColorU8_sRGB aColor )

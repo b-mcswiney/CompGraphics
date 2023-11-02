ed3f8d0b37253f3c53747b056798edf27ddb1ec4
Brooklyn Mcswiney <brooklynmcswiney@gmail.com>
2023-11-02 16:43:55 +0000

    built a release version

---
+++ cw1/_build_/release-x64-gcc/x64/release/x-benchmark/check.d
@@ -0,0 +1,8 @@
+../_build_/release-x64-gcc/x64/release/x-benchmark/check.o: \
+ benchmark/src/check.cc benchmark/src/check.h \
+ benchmark/include/benchmark/export.h benchmark/src/internal_macros.h \
+ benchmark/src/log.h
+benchmark/src/check.h:
+benchmark/include/benchmark/export.h:
+benchmark/src/internal_macros.h:
+benchmark/src/log.h:

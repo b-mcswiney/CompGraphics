395285a48abbaffb7c8230bf85a8d1afb3c171f1
Brooklyn Mcswiney <brooklynmcswiney@gmail.com>
2023-11-07 15:57:18 +0000

    started work on lines benchmark

--- cw1/draw2d/draw.cpp
+++ cw1/draw2d/draw.cpp
@@ -14,43 +14,103 @@ float find_area_triangle(Vec2f A, Vec2f B, Vec2f C);
 
 void draw_line_solid( Surface& aSurface, Vec2f aBegin, Vec2f aEnd, ColorU8_sRGB aColor )
 {
-	// Calculate D vector
-	float dX = aEnd.x - aBegin.x;
-	float dY = aEnd.y - aBegin.y;
+	// Calculate slope
+	float dx = aEnd.x - aBegin.x;
+	float dy = aEnd.y - aEnd.x;
 
-	// initialize step
-	int step;
+	// Initialise driver
+	float driver;
+	float currentPxl;
 
-	//initialize current x and y values
-	float currentY = aBegin.y;
-	float currentX = aBegin.x;
-
-	// Finding X major or Y Major
-	// Absolute value ensures we never divide by 0
-	if(abs(dX) > abs(dY)) {
-		step = abs(dX);
-	}
-	else {
-		step = abs(dY);
-	}
-
-	// Find step in each iteration
-	float yInc = dY/step;
-	float xInc = dX/step;
-
-	// Set pixels in the line
-	for(int i = 0; i < step; i++)
+	if(abs(dx) > abs(dy))
 	{
-		// Only draw what's within the surface
-		if(currentX >= 0 && currentX < aSurface.get_width()
-			&& currentY >= 0 && currentY < aSurface.get_height()) 
+		driver = dy/dx;
+		currentPxl = aBegin.y;
+	}
+	else
+	{
+		driver = dx/dy;
+		currentPxl = aBegin.x;
+	}
+	int error = abs(driver)-1;
+
+	if(abs(dx)>abs(dy))
+	{
+		for(int i = aBegin.x; i <= aEnd.x; i++)
+		{
+			if(i >= 0 && i < (float)aSurface.get_width()
+				&& currentPxl >= 0 && currentPxl < (float)aSurface.get_height()) 
 			{
-				aSurface.set_pixel_srgb(currentX, currentY, aColor);
+				aSurface.set_pixel_srgb(i, currentPxl, aColor);
 			}
 
-		currentY += yInc;
-		currentX += xInc;
+			if(error >= 0)
+			{
+				currentPxl += 1;
+				error -= 1;
+			}
+
+			error += driver;
+		}
 	}
+	else
+	{
+		for(int i = aBegin.y; i <= aEnd.y; i++)
+		{
+			if(currentPxl >= 0 && currentPxl < (float)aSurface.get_width()
+				&& i >= 0 && i < (float)aSurface.get_height()) 
+			{
+				aSurface.set_pixel_srgb(currentPxl, i, aColor);
+			}
+
+			if(error >= 0)
+			{
+				currentPxl += 1;
+				error -= 1;
+			}
+
+			error += driver;
+		}
+	}
+	
+
+	// // Calculate D vector
+	// float dX = aEnd.x - aBegin.x;
+	// float dY = aEnd.y - aBegin.y;
+
+	// // initialize step
+	// int step;
+
+	// //initialize current x and y values
+	// float currentY = aBegin.y;
+	// float currentX = aBegin.x;
+
+	// // Finding X major or Y Major
+	// // Absolute value ensures we never divide by 0
+	// if(abs(dX) > abs(dY)) {
+	// 	step = abs(dX);
+	// }
+	// else {
+	// 	step = abs(dY);
+	// }
+
+	// // Find step in each iteration
+	// float yInc = dY/step;
+	// float xInc = dX/step;
+
+	// // Set pixels in the line
+	// for(int i = 0; i < step; i++)
+	// {
+	// 	// Only draw what's within the surface
+	// 	if(currentX >= 0 && currentX < aSurface.get_width()
+	// 		&& currentY >= 0 && currentY < aSurface.get_height()) 
+	// 		{
+	// 			aSurface.set_pixel_srgb(currentX, currentY, aColor);
+	// 		}
+
+	// 	currentY += yInc;
+	// 	currentX += xInc;
+	// }
 }
 
 void draw_triangle_wireframe( Surface& aSurface, Vec2f aP0, Vec2f aP1, Vec2f aP2, ColorU8_sRGB aColor )
--- cw1/lines-benchmark/main.cpp
+++ cw1/lines-benchmark/main.cpp
@@ -3,11 +3,50 @@
 #include "../draw2d/draw.hpp"
 #include "../draw2d/surface.hpp"
 
-namespace
+void draw_line_bresenham( Surface& aSurface, Vec2f aBegin, Vec2f aEnd, ColorU8_sRGB aColor )
 {
-	// This is a placeholder. Replace this with yor own code. Refer to
-	// blit-benchmark/main.cpp for a more complete example. 
-	void placeholder_( benchmark::State& aState )
+	// Calculate D vector
+	float dX = aEnd.x - aBegin.x;
+	float dY = aEnd.y - aBegin.y;
+
+	// initialize step
+	int step;
+
+	//initialize current x and y values
+	float currentY = aBegin.y;
+	float currentX = aBegin.x;
+
+	// Finding X major or Y Major
+	// Absolute value ensures we never divide by 0
+	if(abs(dX) > abs(dY)) {
+		step = abs(dX);
+	}
+	else {
+		step = abs(dY);
+	}
+
+	// Find step in each iteration
+	float yInc = dY/step;
+	float xInc = dX/step;
+
+	// Set pixels in the line
+	for(int i = 0; i < step; i++)
+	{
+		// Only draw what's within the surface
+		if(currentX >= 0 && currentX < aSurface.get_width()
+			&& currentY >= 0 && currentY < aSurface.get_height()) 
+			{
+				aSurface.set_pixel_srgb(currentX, currentY, aColor);
+			}
+
+		currentY += yInc;
+		currentX += xInc;
+	}
+}
+
+namespace
+{	
+	void DDA_benchmark_diagonal( benchmark::State& aState)
 	{
 		auto const width = std::uint32_t(aState.range(0));
 		auto const height = std::uint32_t(aState.range(1));
@@ -17,8 +56,34 @@ namespace
 
 		for( auto _ : aState )
 		{
-			// Placeholder that just does something:
-			surface.clear(); // PLACEHOLDER! EXCLUDE FROM REAL BENCHMARKS!
+			// Draw line from corner to corner
+			draw_line_solid( surface, 
+							{0, 0}, 
+							{(float)surface.get_width(), (float)surface.get_height()}, 
+							{255, 255, 255}
+						);
+
+			// ClobberMemory() ensures that the compiler won't optimize away
+			// our blit operation. (Unlikely, but technically poossible.)
+			benchmark::ClobberMemory(); 
+		}
+	}
+	void bresenham_benchmark_diagonal( benchmark::State& aState)
+	{
+		auto const width = std::uint32_t(aState.range(0));
+		auto const height = std::uint32_t(aState.range(1));
+
+		Surface surface( width, height );
+		surface.clear();
+
+		for( auto _ : aState )
+		{
+			// Draw line from corner to corner
+			draw_line_bresenham( surface, 
+								{0, 0}, 
+								{(float)surface.get_width(), (float)surface.get_height()}, 
+								{255, 255, 255}
+							);
 
 			// ClobberMemory() ensures that the compiler won't optimize away
 			// our blit operation. (Unlikely, but technically poossible.)
@@ -27,10 +92,14 @@ namespace
 	}
 }
 
-BENCHMARK( placeholder_ )
-	->Args( { 1920, 1080 } )
+
+BENCHMARK( DDA_benchmark_diagonal )
+	->Args( { 320, 240 } )
 	->Args( { 7680, 4320 } )
 ;
-
+BENCHMARK( bresenham_benchmark_diagonal )
+	->Args( { 320, 240 } )
+	->Args( { 7680, 4320 } )
+;
 
 BENCHMARK_MAIN();
